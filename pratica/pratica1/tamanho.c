6ccc022453b60be2989179548e4d053967f3560a
JoaoTroianoSegoria <160737815+JoaoTroianoSegoria@users.noreply.github.com>
2024-08-05 13:59:09 +0000

    Initial commit

---
+++ pratica/pratica1/tamanho.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//representação de ponteiros
+int main (void) {
+  int x = 5;
+  float pi =3.14;
+  int *ptr_x;
+  ptr_x = &x;
+  int *ptr_pi;
+  printf("&x: %p, x: %d\n", &x, x);
+  printf("&ptr_x: %p, ptr_x: %p, *ptr_x: %d\n",
+          &ptr_x, ptr_x, *ptr_x);
+
+  //Como alterar o valor de x para 7 utilizando ptr_x?
+  ptr_x = &x;
+  *ptr_x = 7;
+  printf("O valor de x é %d\n", *ptr_x);
+  
+
+  
+  return 0;
+}
